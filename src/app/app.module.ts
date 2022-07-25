2f486dfc65b5f2664395ee52e6e10e53941da08b
Nagarjuna-Singamsetti <naga0955@gmail.com>
2022-07-25 15:39:32 +0530

    AAA pattern Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketReservationComponent } from './components/ticket-reservation/ticket-reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketReservationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ticket-reservation/ticket-reservation.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TicketReservationComponent } from './ticket-reservation.component';
+
+describe('TicketReservationComponent', () => {
+  let component: TicketReservationComponent;
+  let fixture: ComponentFixture<TicketReservationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TicketReservationComponent]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TicketReservationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('Ticket Reservation', () => {
+  it('Testing Reservation is Done Or Not', () => {
+    //Arrange
+    let Reservation = new TicketReservationComponent;
+    //Act
+    let isReserved = Reservation.reserveSeat();
+    //Assert
+    expect(isReserved).toBeTruthy();
+  });
+});
