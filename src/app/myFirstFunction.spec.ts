3a33dc3e571900d6fbbd59efb2ea710f4a2dc852
Nagarjuna-Singamsetti <naga0955@gmail.com>
2022-07-25 11:35:13 +0530

    Added Simple Subtraction TestCase

--- src/app/myFirstFunction.spec.ts
+++ src/app/myFirstFunction.spec.ts
@@ -1,7 +1,10 @@
-import { addition } from "./myFirstFunction"
+import { addition, subtraction } from "./myFirstFunction"
 
-describe('Test Addition',()=>{
+describe('Test Functions',()=>{
     it('Testing Addition Function',()=>{
-        expect(addition(20,25)).toBe(45);
-    })
+        expect(addition(25,20)).toBe(45);
+    });
+    it('Testing Subtraction Function',()=>{
+        expect(subtraction(25,20)).toBe(5);
+    });
 })
